f5c76acf7212edbf8b33851b4d6f9f34b27b1ec0
Lennin <lenninlasd@gmail.com>
2016-04-13 20:12:14 -0500

    testing maps in modals

---
+++ src/common/mapPreview/mapPreview.js
@@ -0,0 +1,31 @@
+(function() {
+  angular.module('loom_map_preview', [])
+  .directive('loomMappreview', loomMappreviewDirective);
+
+  function loomMappreviewDirective() {
+    return {
+      scope: {},
+      controller: loomMappreviewCtrl,
+      template: '<div id="maapa"></div>'
+    };
+  }
+  var map;
+  function loomMappreviewCtrl() {
+    map = new ol.Map({
+      target: 'maapa',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+
+    map.on('dragend', function() {
+    });
+  }
+
+})();
