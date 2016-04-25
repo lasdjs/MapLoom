71433cbb9e64ae11733263a0ade1ea555581daef
zenerlasd <lenninlasd@gmail.com>
2016-04-25 18:23:14 -0500

    test init

--- src/common/mapPreview/mapPreview.js
+++ src/common/mapPreview/mapPreview.js
@@ -17,50 +17,122 @@
 
     $('#add-layer-dialog').on('shown.bs.modal', function() {
       if (map === undefined) {
-        // runMap();
-        mapService.runMap('maapa');
+        runMap();
+        //mapService.runMap('maapa');
         map = true;
       }
     });
 
     function runMap() {
-      // var osmSource = new ol.source.OSM();
-      var layers = [
-        // new ol.layer.Tile({
-        //   source: osmSource
-        // }),
-        // new ol.layer.Tile({
-        //   source: new ol.source.TileDebug({
-        //     projection: 'EPSG:3857',
-        //     tileGrid: osmSource.getTileGrid()
-        //   })
-        // }),
-        new ol.layer.Tile({
-          source: new ol.source.XYZ({
-            url: 'http://{a-c}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png'
+
+      var geojsonFormat = new ol.format.GeoJSON();
+
+      var vectorSource = new ol.source.Vector({
+        loader: function(extent, resolution, projection) {
+          var url = 'http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/' +
+              'Petroleum/KSFields/FeatureServer/0/query?f=json&returnGeometry=' +
+              'true&spatialRel=esriSpatialRelIntersects&geometry=' +
+              encodeURIComponent('{"xmin":' + extent[0] + ',"ymin":' +
+                  extent[1] + ',"xmax":' + extent[2] + ',"ymax":' + extent[3] +
+                  ',"spatialReference":{"wkid":102100}}') +
+              '&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*' +
+              '&outSR=102100&callback=loadFeatures';
+          // use jsonp: false to prevent jQuery from adding the "callback"
+          // parameter to the URL
+          $.ajax({url: url, dataType: 'jsonp', jsonp: false});
+        },
+        strategy: ol.loadingstrategy.tile(new ol.tilegrid.createXYZ({
+          maxZoom: 19,
+          tileSize: 512
+        }))
+      });
+
+      var loadFeatures = function(response) {
+        var features = [];
+        for (var i = 0, ii = response.features.length; i < ii; ++i) {
+          var primitive = Terraformer.ArcGIS.parse(response.features[i]);
+          var olFeature = geojsonFormat.readFeature(primitive);
+          features.push(olFeature);
+        }
+        if (features.length > 0) {
+          vectorSource.addFeatures(features);
+        }
+      };
+
+      var styleCache = {
+        'ABANDONED': [
+          new ol.style.Style({
+            fill: new ol.style.Fill({
+              color: 'rgba(225, 225, 225, 255)'
+            }),
+            stroke: new ol.style.Stroke({
+              color: 'rgba(0, 0, 0, 255)',
+              width: 0.4
+            })
           })
-        }),
-        new ol.layer.Tile({
-          source: new ol.source.TileJSON({
-            url: 'http://api.tiles.mapbox.com/v3/' +
-                'mapbox.20110804-hoa-foodinsecurity-3month.jsonp',
-            crossOrigin: 'anonymous'
+        ],
+        'GAS': [
+          new ol.style.Style({
+            fill: new ol.style.Fill({
+              color: 'rgba(255, 0, 0, 255)'
+            }),
+            stroke: new ol.style.Stroke({
+              color: 'rgba(110, 110, 110, 255)',
+              width: 0.4
+            })
           })
+        ],
+        'OIL': [
+          new ol.style.Style({
+            fill: new ol.style.Fill({
+              color: 'rgba(56, 168, 0, 255)'
+            }),
+            stroke: new ol.style.Stroke({
+              color: 'rgba(110, 110, 110, 255)',
+              width: 0
+            })
+          })
+        ],
+        'OILGAS': [
+          new ol.style.Style({
+            fill: new ol.style.Fill({
+              color: 'rgba(168, 112, 0, 255)'
+            }),
+            stroke: new ol.style.Stroke({
+              color: 'rgba(110, 110, 110, 255)',
+              width: 0.4
+            })
+          })
+        ]
+      };
+
+      var vector = new ol.layer.Vector({
+        source: vectorSource,
+        style: function(feature, resolution) {
+          var classify = feature.get('activeprod');
+          return styleCache[classify];
+        }
+      });
+
+      var attribution = new ol.Attribution({
+        html: 'Tiles &copy; <a href="http://services.arcgisonline.com/ArcGIS/' +
+            'rest/services/World_Topo_Map/MapServer">ArcGIS</a>'
+      });
+
+      var raster = new ol.layer.Tile({
+        source: new ol.source.XYZ({
+          attributions: [attribution],
+          url: 'http://server.arcgisonline.com/ArcGIS/rest/services/' +
+              'World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
         })
-        // new ol.layer.Tile({
-        //   source: new ol.source.TileJSON({
-        //     url: 'http://api.tiles.mapbox.com/v3/' +
-        //         'mapbox.world-borders-light.jsonp',
-        //     crossOrigin: 'anonymous'
-        //   })
-        // })
-      ];
+      });
+
       map = new ol.Map({
         target: 'maapa',
-        layers: layers,
+        layers: [raster, vector],
         view: new ol.View({
-          center: [-472202, 7530279],
-          zoom: 2
+          center: ol.proj.transform([-97.6114, 38.8403], 'EPSG:4326', 'EPSG:3857'),
+          zoom: 7
         })
       });
       map.on('dragend', function() {
